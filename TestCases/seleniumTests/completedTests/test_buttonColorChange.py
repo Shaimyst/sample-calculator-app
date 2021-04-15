ea49f9facfe19ebed950488e33d2864d9d8a91d1
Jessica Sadler <jessicagsadler@gmail.com>
2021-04-15 17:06:42 +0200

    completed test for checking button colors before and after clicking

--- TestCases/seleniumTests/completedTests/test_buttonColorChange.py
+++ TestCases/seleniumTests/completedTests/test_buttonColorChange.py
@@ -15,20 +15,17 @@ driver.get("http://localhost:5000")
 #maximize window
 driver.maximize_window
 
-# loop for finding all buttons and printing buttons clicked
-# tags = driver.find_elements_by_tag_name('button')
+# loop for finding all buttons and button colors before/after click
+tags = driver.find_elements_by_tag_name('button')
 
-# for t in tags:
-#     t.click()
-#     print("clicked: " + t.text + " is a " + t.tag_name)
-
-# 7 xpath is /html/body/main/div/div[2]/div[1]/button
-
-# get css value button color
-# click
-# get color
-
-# return any errors
+for t in tags:
+    color1 = t.value_of_css_property("background-color")
+    t.click()
+    color2 = t.value_of_css_property("background-color")
+    print("Background colors are:" + color1 + color2)
 
 #close browser
 driver.quit()
+
+# assertion
+assert color1 is not color2, "No color change."
