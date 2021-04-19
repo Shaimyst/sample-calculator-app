4be5f784d5d77e7aa2a73145a4acc475032b3b9c
Jessica Sadler <jessicagsadler@gmail.com>
2021-04-19 11:32:13 +0200

    completed selenium all buttons test

--- TestCases/seleniumTests/test_allButtons.py
+++ TestCases/seleniumTests/test_allButtons.py
@@ -20,14 +20,21 @@ driver.maximize_window
 
 tags = driver.find_elements_by_tag_name('button')
 
+count = 0
+
 for t in tags:
     t.click()
+    count += 1 # variable will increment every loop iteration of your code
     tagname = t.tag_name
-    print(t.text + " is a " + t.tag_name)
+    # option: print list of all buttons clicked
+    # print(t.text + " is a " + t.tag_name)
+
+print("There are " + str(count) + " buttons")
 
 #close browser
 driver.quit()
 
-# change
-assert tagname == "button", "Not a button"
-# calc should have a certain amount of buttons have assertion report the correct amount.
+# calc should have a 16 buttons have assertion report the correct amount.
+assert count == 16, "There are not 16 buttons"
+assert count > 15, "There are buttons missing"
+assert count < 17, "There are too many buttons"
