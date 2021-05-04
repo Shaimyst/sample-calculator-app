8bb7ecb36beb70fb4c1e789759a4d69984600430
Jessica Sadler <jessicagsadler@gmail.com>
2021-05-04 11:08:40 +0200

    finished testSelenium.py

--- TestCases/seleniumTests/testSelenium.py
+++ TestCases/seleniumTests/testSelenium.py
@@ -1,6 +1,8 @@
 import os
 from selenium import webdriver
 
+# a starter test to make sure selenium and chromedriver are working
+
 chromedriver = "/usr/local/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
