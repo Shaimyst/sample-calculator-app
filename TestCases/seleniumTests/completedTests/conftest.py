5b61d79dad2a7968d513a582eeae535a4193147e
Jessica Sadler <jessicagsadler@gmail.com>
2021-06-29 10:35:26 +0200

    moved conftest.py

---
+++ TestCases/seleniumTests/completedTests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture(scope="session")
+def browserdriver():
+   driver = webdriver.Chrome(ChromeDriverManager().install())
+   yield driver
+   driver.quit()
